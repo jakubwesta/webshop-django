6e4787fffef6b30d2d2833475c6a10067783ec97
Jakub Westa <jakub.westa@gmail.com>
2021-05-27 11:07:24 +0200

    Added form to select amount while buying

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django.forms import fields
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
@@ -12,4 +13,24 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from products.models import Product
 
 class BuynowForm(forms.Form):
-    buy_now = forms.CharField()
+    amount = forms.IntegerField()
+
+    def __init__(self, *args, **kwargs):
+        try:
+            self.avaible_amount = kwargs.pop('amount')
+        except:
+            self.avaible_amount = 1
+        super(BuynowForm, self).__init__(*args, **kwargs)
+        self.fields['amount'].widget = forms.NumberInput(attrs={'min': 1,
+                                                                'max': self.avaible_amount,
+                                                                'step': 1,
+                                                                'value': 1})
+
+    def clean_amount(self):
+        amount = self.cleaned_data['amount']
+        if type(amount) != int:
+            raise ValidationError('Wrong amount!')
+        return amount
+
+
+                            
--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import Http404, HttpResponseRedirect
 from django.urls.base import reverse_lazy
 from products.forms import BuynowForm
 from django.shortcuts import redirect, render, resolve_url
 from django.http import HttpResponse
 from django.views import generic
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
 from .models import Product
@@ -14,27 +15,40 @@ from purchases.models import Purchase
 
 class ProductDetailsView(generic.DetailView):
     model = Product
+    user = get_user_model()
     template_name = 'products/product_page.html'
     
     def get_object(self):
         try:
             obj = Product.objects.get(pk=self.kwargs['uuid'])
         except:
-            obj = None
+            raise Http404('Product not found!')
         return obj
 
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailsView, self).get_context_data(**kwargs)
+        obj = self.get_object()
+        if 'buynow_form' not in context:
+            context['buynow_form'] = BuynowForm(amount=obj.amount)
+        return context
+
     def post(self, request, *args, **kwargs):
-        if 'buy_now' in request.POST:
-            buy_now_form = BuynowForm(request.POST)
-            if buy_now_form.is_valid():
-                obj = self.get_object()
-                obj.amount -= 1
+        obj = self.get_object()
+        if 'buynow' in request.POST:
+            if self.request.user not in self.user.objects.all():
+                raise Http404('To buy a product user need to be logged in!')
+            buynow_form = BuynowForm(request.POST, amount=obj.amount)
+            if buynow_form.is_valid():
+                obj.amount -= int(buynow_form.clean_amount())
+                if obj.amount == 0:
+                    obj.publicly_listed = False
                 obj.save()
                 Purchase.objects.create(product=obj,
                                         buyer=self.request.user,
-                                        amount=1,
+                                        amount=int(buynow_form.clean_amount()),
                                         price=obj.buy_now)
                 return redirect('product-details', uuid=self.kwargs['uuid'])
+        
 
 class ProductListView(generic.ListView):
     model = Product
